f4756a311a8df70af99e6595ac91e774c2b450ce
Walter Ojeda <ojeda.97.walter@gmail.com>
2023-04-02 01:12:27 -0300

    tomos

--- JS/main.js
+++ JS/main.js
@@ -13,29 +13,35 @@ if('i' === queGenero){
     cual= prompt("¿De cuales de estos titulos gustaría comprar? \n No Game No Life 'No' Sword Art Online 'Sao' Inuyasha 'Inu'")
 }
 if('ao' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 13"))
 }
 if('na' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 21"))
 }
 if('ni' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 25"))
 }
 if('one' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 89"))
 }
 if('ki' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 23"))
 }
 if('fa' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 63"))
 }
 if('no' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione del 1 al 7"))
 }
 if('sao' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccion del 1 al 27"))
 }
 if('inu' === cual){
-    cuantos= Number(prompt("¿Cuantos tomos gustarias?"))
+    cuantos= Number(prompt("¿Cuantos tomos gustarias? Seleccione dle 1 al 56"))
+}
+if(cuantos>=5){
+    alert("En breve estaremos comunicandonos con usted")
+}
+else if(cuantos<=4){
+    alert("La compra minima son de 5 ejemplares")
 }
